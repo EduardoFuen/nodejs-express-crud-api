8afc43b20a36048ace05fa8a18ffe957949a75b1
EduardoFuen <destructok1505@gmail.com>
2025-03-25 17:36:30 -0400

    env2
    tgea

--- routes/product.js
+++ routes/product.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const AWS = require('aws-sdk');
 AWS.config.update({
   region: 'us-east-1',
-  accessKeyId: process.env[AWS_ACCESS_KEY_ID],
-  secretAccessKey: process.env[AWS_SECRET_ACCESS_KEY],
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const dynamodbTableName = 'TestTable';
