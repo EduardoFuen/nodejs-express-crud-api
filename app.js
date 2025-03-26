d4c31e9b308dd44431f4201be85391868bbe2e94
EduardoFuen <destructok1505@gmail.com>
2025-03-26 00:23:20 -0400

    te4asd
    tasd

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const port = process.env.PORT || 3050;
 
 const productRouter = require('./routes/product');
 const warehouseRouter = require('./routes/warehouse');
-
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+//app.use(express.urlencoded({limit: '50mb'}));
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use('/product', productRouter);
 app.use('/warehouse', warehouseRouter);
 
