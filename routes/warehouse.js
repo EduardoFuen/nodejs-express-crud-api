c5d8fdc63946e2bcc3da45c8abac00640c83dce1
EduardoFuen <destructok1505@gmail.com>
2025-03-26 22:21:42 -0400

    test
    testv23

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -6,6 +6,7 @@ AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
+console.log(process.env.AWS_ACCESS_KEY_ID)
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const dynamodbTableName = 'TestTable';
 
